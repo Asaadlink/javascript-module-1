43354c8f39b1180da3e7d563462cb4c7bc180d14
Asaadlink <Asaadfit@gmail.com>
2022-03-05 16:22:31 +0100

    done

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,23 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+function blackBox(num) {
+  if (isEven(num)) {
+    console.log(`The exponential of ${num} is ${exponential(num)}.`);
+  }
+}
+
+for (let num = 5; num <= 20; num++) {
+  blackBox(num);
+}
+// 5-20
+
+let num = 5
+while (num <= 20) {
+  blackBox(num);
+  num++;
+}
+
+
+// console.log(`The exponential of ${num} is ${exponential(num)}.`);
